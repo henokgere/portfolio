4be205432e18d7402d1ff99851e4e67e0fa79106
henokgere <henok1234gere@gmail.com>
2024-11-09 11:01:01 +0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,15 @@ function App() {
     {names: ['Git', 'Github', 'Docker', 'Agile', 'Scrum']},
   ]
   return (
-    <div>
+    <div style={{display: 'grid'}}>
       <div className="top">
         <div className="nav">
           <div></div>
-          <i class="fa-brands fa-linkedin"></i> 
-          <i class="fa-brands fa-upwork"></i>
-          <i class="fa-brands fa-figma"></i>
-          <i class="fa-brands fa-email"></i>
+          <a href="https://www.linkedin.com/in/henok-gebremedhin-74a3b3197?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><i class="fa-brands fa-linkedin"></i></a>
+          <a href="https://github.com/henokgere"><i class="fa-brands fa-github"></i></a>
+          <a href="https://www.upwork.com/freelancers/~0191c6f167ad705df8?mp_source=share"><i class="fa-brands fa-upwork"></i></a>
+          <a href="https://www.figma.com/files/team/1307218504960877386/recents-and-sharing?fuid=1285919460291721930"><i class="fa-brands fa-figma"></i></a>
+          <a href="henok1234gere@gmail.com"><i class="fa-brands fa-email"></i></a>
         </div>
         <div className="title">
           <h1><b>Henok is a</b> web and mobile app designer and full stack developer</h1>
@@ -88,9 +89,14 @@ function App() {
           </div>
         ))}
       </div>
-      <footer>
+      <div className="bottomLine">
+        <h1># </h1>
+        <div></div>
+      </div>
+      <p className='bottom'>I’m interested in freelance opportunities. However, if you have other request or question, don’t hesitate to contact me</p>
+      <div className='footer'>
         <p>&copy; this is made by Henok G Nov 2024</p>
-      </footer>
+      </div>
     </div>
   )
 }
